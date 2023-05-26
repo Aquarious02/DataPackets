f1bbf3aba4fd5c39d877a8170d359de639deb4d8
Aquarious <45901188+Aquarious02@users.noreply.github.com>
2023-05-26 23:06:04 +0300

    style

--- tests/packets.py
+++ tests/packets.py
@@ -4,30 +4,36 @@ from data_packets.packets import *
 
 
 class PacketTest(Block):
-    _fields_ = [('full_word', c_uint16, 16),
-                ('half_word_1', c_uint16, 8),
-                ('half_word_2', c_uint16, 8),
-                ('bit9', c_uint16, 9),
-                ('bit5', c_uint16, 5),
-                ('bit1', c_uint16, 1),
-                ('bit15', c_uint16, 15)]
+    _fields_ = [
+        ('full_word', c_uint16, 16),
+        ('half_word_1', c_uint16, 8),
+        ('half_word_2', c_uint16, 8),
+        ('bit9', c_uint16, 9),
+        ('bit5', c_uint16, 5),
+        ('bit1', c_uint16, 1),
+        ('bit15', c_uint16, 15),
+    ]
 
-    _field_names_ = [('full_word', 'Test 16 bit'),
-                     ('half_word_1', 'Test 8_1 bit'),
-                     ('half_word_2', 'Test 8_2 bit'),
-                     ('bit9', 'Test 9 bit'),
-                     ('bit5', 'Test 5 bit'),
-                     ('bit1', 'Test 1 bit'),
-                     ('bit15', 'Test 15 bit')]
+    _field_names_ = [
+        ('full_word', 'Test 16 bit'),
+        ('half_word_1', 'Test 8_1 bit'),
+        ('half_word_2', 'Test 8_2 bit'),
+        ('bit9', 'Test 9 bit'),
+        ('bit5', 'Test 5 bit'),
+        ('bit1', 'Test 1 bit'),
+        ('bit15', 'Test 15 bit'),
+    ]
 
 
-test_fields = [Field(item_name='Test 16 bit', attribute_name='full_word', meaning=0, bit_length=16),
-               Field(item_name='Test 8_1 bit', attribute_name='half_word_1', meaning=0, bit_length=8),
-               Field(item_name='Test 8_2 bit', attribute_name='half_word_2', meaning=0, bit_length=8),
-               Field(item_name='Test 9 bit', attribute_name='bit9', meaning=0, bit_length=9),
-               Field(item_name='Test 7 bit', attribute_name='bit7', meaning=0, bit_length=7),
-               Field(item_name='Test 1 bit', attribute_name='bit1', meaning=0, bit_length=1),
-               Field(item_name='Test 15 bit', attribute_name='bit15', meaning=0, bit_length=15)]
+test_fields = [
+    Field(item_name='Test 16 bit', attribute_name='full_word', meaning=0, bit_length=16),
+    Field(item_name='Test 8_1 bit', attribute_name='half_word_1', meaning=0, bit_length=8),
+    Field(item_name='Test 8_2 bit', attribute_name='half_word_2', meaning=0, bit_length=8),
+    Field(item_name='Test 9 bit', attribute_name='bit9', meaning=0, bit_length=9),
+    Field(item_name='Test 7 bit', attribute_name='bit7', meaning=0, bit_length=7),
+    Field(item_name='Test 1 bit', attribute_name='bit1', meaning=0, bit_length=1),
+    Field(item_name='Test 15 bit', attribute_name='bit15', meaning=0, bit_length=15)
+]
 
 test_bytes = (1234567891011121314).to_bytes(8, 'big')
 
@@ -53,7 +59,6 @@ class TestBlock(unittest.TestCase):
 
         with self.subTest('Compare test bytes and value in filled_test_block'):
             self.assertEqual(self.filled_test_block.half_word_1, test_bytes[2])
-
 
     def test_get_attr(self):
         result = [hasattr(self.filled_test_block, attr_name) for attr_name in map(lambda x: x.attribute_name, test_fields)]
